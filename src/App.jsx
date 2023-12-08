094431ffb706c0e1774d172ccd3b9d0faf7ffafc
Marcus Perdue <marcusperduee@gmail.com>
2023-12-07 20:48:13 -0800

    added redirect

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// TODO: Add a comment explaining what this import statement is doing
+// imports the HelloReact from './components/HelloReact' file
+import HelloReact from './components/HelloReact';
+
+// TODO: Add a comment explaining the purpose of the App component
+// app is the main entry point
+// renders hello react 
+function App() {
+  return <HelloReact />;
+}
+
+export default App;
---
+++ src/components/HelloReact.jsx
@@ -0,0 +1,12 @@
+// TODO: Add a comment explaining what a react component is
+// reusable user interface
+function HelloReact() {
+  const text = 'some text';
+
+  // TODO: Add a comment explaining what JSX is and the significance of the curly braces
+  // JSX is a html like syntax makes js easier in react
+  // curly braces embedd the variable inside, allowing dynamic content
+  return <h2>Hello World! Here is {text}</h2>;
+}
+
+export default HelloReact;
